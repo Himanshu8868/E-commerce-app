85b207595c9d1b14613b778ede266509a0364cca
himanshu <Himanshurwt@gmail.com>
2025-01-11 23:28:25 +0530

    creating a react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+        <div className="header-container">
+          <Header/>
+        </div>
+    </div>
+  )
+}
+
+export default App
